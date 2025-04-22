667466282a727493f422511dd863e098f0a77fc9
dajmera-24 <dhruvajmera0@gmail.com>
2025-04-22 10:50:57 -0500

    Update SuperpermAlgoPrint.java

--- SuperpermAlgoPrint.java
+++ SuperpermAlgoPrint.java
@@ -1,69 +1,59 @@
-import java.util.*;
-import java.io.PrintWriter;
-import java.io.IOException;
 public class SuperpermAlgoPrint {
 
    private int n;
    private int intersect;
-   private StringBuilder currentP;
+   private StringBuilder bead;
    private PrintWriter writer;
 
    public SuperpermAlgoPrint(char[] c) {
       n = c.length;
-      currentP = new StringBuilder();
-      try {
-         writer = new PrintWriter("superperm_n" + n + ".txt");
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-      startPhrase(c);
+      bead = new StringBuilder();
+      try {writer = new PrintWriter("superperm_n" + n + ".txt");} 
+      catch (IOException e) {e.printStackTrace();}
+      startBead(c);
       algo(2);
       writer.close();
    }
 
-   public void startPhrase(char[] c) {
+   public void startBead(char[] c) {
       for (int i = 0; i < n; i++) {
-         currentP.append(c[i]);
+         bead.append(c[i]);
       }
-      for (int i = 0; i < currentP.length(); i++) {
-         writer.print(currentP.charAt(i));
+      for (int i = 0; i < bead.length(); i++) {
+         writer.print(bead.charAt(i));
       }
-      for (int i = 0; i < currentP.length()-1; i++) {
-         writer.print(currentP.charAt(i));
+      for (int i = 0; i < bead.length()-1; i++) {
+         writer.print(bead.charAt(i));
       }
    }
    
    public void algo(int d) {
-      if (d==n) {
-         return;}
+      if (d==n) {return;}
       for (int i = 0; i < d; i++) {
          algo(d+1);
          intersect = d-1;
-         if (i == d-1) {
-            return;}
-         coinject(d);
+         if (i == d-1) {return;}
+         mirrorShift(d);
       }
    }
 
-   public void coinject(int index) {
+   public void mirrorShift(int index) {
       int k = n-index;
-      String m = currentP.substring(0, k);
+      String m = bead.substring(0, k);
       for (int i = 0; i < n-k-1; i++) {
-         currentP.setCharAt(i, currentP.charAt(i+k));
+         bead.setCharAt(i, bead.charAt(i+k));
       }
       for (int i = n-k-1; i < n-1; i++) {
-         if (m.length() == 0) {
-            break;}
+         if (m.length() == 0) {break;}
          int l = m.length()-1-(i-(n-k-1));
-         if (l < 0) {
-            continue;}
-         currentP.setCharAt(i, m.charAt(l));
+         if (l < 0) {continue;}
+         bead.setCharAt(i, m.charAt(l));
       }
-      for (int i = intersect; i < currentP.length(); i++) {
-         writer.print(currentP.charAt(i));
+      for (int i = intersect; i < bead.length(); i++) {
+         writer.print(bead.charAt(i));
       }
-      for (int i = 0; i < currentP.length()-1; i++) {
-         writer.print(currentP.charAt(i));
+      for (int i = 0; i < bead.length()-1; i++) {
+         writer.print(bead.charAt(i));
       }
    }
 }
